d3117eb4c5522688b1e4fbaef253d0bcff1211fd
Felix Gonzalez <felixgonzalez@Felixs-MBP-2.lan>
2025-01-17 12:05:42 -0600

    Added basic operations: add, subtract, multiply, and divide

--- calculator.py
+++ calculator.py
@@ -1,2 +1,14 @@
 print("Hi there! Welcome to the Simple Calculator!")
 print("This program will help you perform basic arithmetic operations.")
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply (a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Error: division by 0 is not allowed"
+    else:
+        return a / b
